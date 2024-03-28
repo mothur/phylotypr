6d206f9d22969a9654ff1eb279343fe2d8740fb6
Pat Schloss <pschloss@umich.edu>
2024-03-28 10:47:09 -0400

    Calculate word specific priors and genus conditional probabilities

--- R/kmers.R
+++ R/kmers.R
@@ -29,3 +29,54 @@ base4_to_index <- function(base4_str) {
   # adding 1 to all base10 values
   na.omit(strtoi(base4_str, base = 4) + 1) |> as.numeric()
 }
+
+
+detect_kmers <- function(sequence, kmer_size = 8) {
+
+  kmers <- get_all_kmers(sequence, kmer_size)
+  indices <- base4_to_index(kmers)
+
+  n_kmers <- 4^kmer_size
+
+  kmers_detected <- numeric(n_kmers)
+  kmers_detected[indices] <- 1
+
+  return(kmers_detected)
+}
+
+
+detect_kmers_across_sequences <- function(sequences, kmer_size = 8) {
+# kmer_size <- 3
+#   detect_kmers(sequences[1], kmer_size)
+#   detect_kmers(sequences[2], kmer_size)
+
+  sapply(sequences, detect_kmers, kmer_size = kmer_size, USE.NAMES = FALSE)
+}
+
+
+calc_word_specific_priors <- function(detect_matrix) {
+
+  (apply(detect_matrix, 1, sum) + 0.5) / (1 + ncol(detect_matrix))
+
+}
+
+
+#(m(wi) + Pi) / (M + 1)
+calc_genus_conditional_prob <- function(detect_matrix,
+                                        genera, #needs to be an integer not char
+                                        word_specific_priors) {
+
+  genus_counts <- table(genera) |> as.vector()
+  n_genera <- length(genus_counts)
+  n_sequences <- length(genera)
+
+  genus_count <- matrix(0,
+                        nrow = nrow(detect_matrix),
+                        ncol = n_genera)
+
+  for(i in 1:n_sequences) {
+    genus_count[, genera[i]] <- detect_matrix[, i] + genus_count[, genera[i]]
+  }
+
+  t(t(genus_count + word_specific_priors) / (genus_counts + 1))
+}
--- tests/testthat/test-kmers.R
+++ tests/testthat/test-kmers.R
@@ -96,3 +96,104 @@ test_that("Generate base10 values from kmers in base4", {
   expect_equal(actual, expected)
 
 })
+
+
+test_that("Accurately detect kmers from a sequence", {
+
+  sequence <- "03212130230210321"
+  kmers <- get_all_kmers(sequence)
+  indices <- base4_to_index(kmers)
+
+  detected <- detect_kmers(sequence)
+
+  expect_equal(length(detected[indices]), length(indices))
+  expect_equal(length(detected[-indices]), 4^8 - length(indices))
+  expect_equal(sum(detected[indices]), length(indices))
+
+
+  sequence <- "03212130230210321N"
+  kmers <- get_all_kmers(sequence)
+  indices <- base4_to_index(kmers)
+
+  detected <- detect_kmers(sequence)
+
+  expect_equal(length(detected[indices]), length(indices))
+  expect_equal(length(detected[-indices]), 4^8 - length(indices))
+  expect_equal(sum(detected[indices]), length(indices))
+
+
+  sequence <- "03212130230210321N"
+  kmers <- get_all_kmers(sequence, kmer_size = 7)
+  indices <- base4_to_index(kmers)
+
+  detected <- detect_kmers(sequence, kmer_size = 7)
+
+  expect_equal(length(detected[indices]), length(indices))
+  expect_equal(length(detected[-indices]), 4^7 - length(indices))
+  expect_equal(sum(detected[indices]), length(indices))
+
+
+})
+
+test_that("Accurately detect kmers across multiple sequences", {
+
+  kmer_size <- 3
+  sequences <- c("03212130", "03212131")
+
+  expected <- matrix(0, nrow = 4^kmer_size, ncol = 2)
+  expected[base4_to_index(get_all_kmers(sequences[1], kmer_size)), 1] <- 1
+  expected[base4_to_index(get_all_kmers(sequences[2], kmer_size)), 2] <- 1
+
+  detect_matrix <- detect_kmers_across_sequences(sequences, kmer_size)
+
+  expect_equal(detect_matrix, expected)
+})
+
+test_that("Calcuate word specific priors", {
+
+  kmer_size <- 3
+  sequences <- c("03212130", "03212131", "03212131")
+  detect_matrix <- detect_kmers_across_sequences(sequences, kmer_size)
+
+  #26 - all 3 = (3+0.5) / (1 + 3) =0.875
+  #29 - only 1 = 0.375
+  #30 - only 2 and 3 = 0.625
+  #64 - none = 0.125
+
+  expected <- (apply(detect_matrix, 1, sum) + 0.5) / (1 + length(sequences))
+  priors <- calc_word_specific_priors(detect_matrix)
+
+  expect_equal(priors, expected)
+  expect_equal(priors[26], 0.875)
+  expect_equal(priors[29], 0.375)
+  expect_equal(priors[30], 0.625)
+  expect_equal(priors[64], 0.125)
+
+})
+
+
+test_that("Calculate genus-specific conditional probabilities", {
+
+  kmer_size <- 3
+  sequences <- c("03212130", "03212131", "03212131")
+  genera <- c(1, 2, 2)
+
+  detect_matrix <- detect_kmers_across_sequences(sequences, kmer_size)
+  priors <- calc_word_specific_priors(detect_matrix)
+
+  #(m(wi) + Pi) / (M + 1)
+  #26 - all 3 = (c(1, 2)+0.5) / (c(1, 2) + 1) = 0.74 & 0.8333333
+  #29 - only 1 = (c(1, 0)+0.5) / (c(1, 2) + 1) = 0.7500000 0.1666667
+  #30 - only 2 and 3 = (c(0, 2)+0.5) / (c(1, 2) + 1) = 0.2500000 0.8333333
+  #64 - none = 0.125 = (c(0, 2)+0.5) / (c(1, 2) + 1) = 0.2500000 0.1666667
+
+  conditional_prob <- calc_genus_conditional_prob(detect_matrix,
+                                                  genera,
+                                                  priors)
+
+  expect_equal(conditional_prob[26,], (c(1, 2)+0.875) / (c(1, 2) + 1))
+  expect_equal(conditional_prob[29,], (c(1, 0)+0.375) / (c(1, 2) + 1))
+  expect_equal(conditional_prob[30,], (c(0, 2)+0.625) / (c(1, 2) + 1))
+  expect_equal(conditional_prob[64,], (c(0, 0)+0.125) / (c(1, 2) + 1))
+
+})
