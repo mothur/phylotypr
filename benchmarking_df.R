526160fed6a5c227de6a79df0772d457eb9f48ba
Pat Schloss <pschloss@umich.edu>
2024-04-19 17:41:24 -0400

    Benchmark accessing data frame like structures

--- benchmarking_df.R
+++ benchmarking_df.R
@@ -208,3 +208,132 @@ microbenchmark(df_grow_rbind(n),
   group_by(expr) %>%
   summarize(median_time = median(time)) %>%
   arrange(-median_time)
+
+n <- 1000
+set.seed(19760620)
+df <- df_predefine(n)
+set.seed(19760620)
+dt <- dt_predefine(n)
+set.seed(19760620)
+tbl <- tbl_predefine(n)
+set.seed(19760620)
+mfull <- matrix_predefine(n)
+set.seed(19760620)
+msparse <- matrix_sparseT(n)
+set.seed(19760620)
+lst <- list_predefine(n)
+
+get_df_single <- function(){
+  df[df$kmer == 20,]
+}
+
+get_df_three <- function(){
+  df[df$kmer == 20 | df$kmer == 30 | df$kmer == 50,]
+}
+
+get_dt_single <- function(){
+  dt[kmer == 20,]
+}
+
+get_dt_three <- function(){
+  dt[kmer == 20 | kmer == 30 | kmer == 50,]
+}
+
+get_tbl_single <- function(){
+  filter(tbl, kmer == 20)
+}
+
+get_tbl_three <- function(){
+  filter(tbl, kmer == 20 | kmer == 30 | kmer == 50)
+}
+
+get_tbl_threeJ <- function(){
+  filter_table <- tibble(kmer = c(20, 30, 50))
+  inner_join(tbl, filter_table, by = "kmer")
+}
+
+get_mfull_single <- function(){
+  mfull[20,]
+}
+
+get_mfull_three <- function(){
+  mfull[c(20, 30, 50),]
+}
+
+
+get_msparse_single <- function(){
+  msparse[20,]
+}
+
+get_msparse_three <- function(){
+  msparse[c(20, 30, 50),]
+}
+
+kmer_vec <- lst$kmer
+genus_vec <- lst$genus
+count_vec <- lst$count
+
+get_vector_single <- function() {
+  kmer_present <- kmer_vec == 20
+  list(kmer = kmer_vec[kmer_present],
+       genus = genus_vec[kmer_present],
+       count = count_vec[kmer_present])
+}
+
+get_vector_threeOR <- function() {
+  kmer_present <- kmer_vec == 20 | kmer_vec == 30 | kmer_vec == 50
+  list(kmer = kmer_vec[kmer_present],
+       genus = genus_vec[kmer_present],
+       count = count_vec[kmer_present])
+}
+
+get_vector_threeIN <- function() {
+  kmer_present <- kmer_vec %in% c(20, 30, 50)
+
+  list(kmer = kmer_vec[kmer_present],
+       genus = genus_vec[kmer_present],
+       count = count_vec[kmer_present])
+}
+
+get_vector_str2lang <- function() {
+  kmer_present <- eval(str2lang(paste(paste("kmer_vec == ", c(20, 30, 50)),
+                                      collapse = " | ")))
+  kmer_vec[kmer_present]
+}
+
+
+get_which_single <- function(){
+  kmer_present <- which(kmer_vec == 20)
+  list(kmer = kmer_vec[kmer_present],
+       genus = genus_vec[kmer_present],
+       count = count_vec[kmer_present])
+}
+
+get_which_three <- function(){
+  kmer_present <- which(kmer_vec == 20 | kmer_vec == 30 | kmer_vec == 50)
+  list(kmer = kmer_vec[kmer_present],
+       genus = genus_vec[kmer_present],
+       count = count_vec[kmer_present])
+}
+
+microbenchmark(get_df_single(),
+               get_dt_single(),
+               get_tbl_single(),
+               get_mfull_single(),
+               get_msparse_single(),
+               get_vector_single(),
+               get_which_single(),
+
+               get_df_three(),
+               get_dt_three(),
+               get_tbl_three(),
+               get_tbl_threeJ(),
+               get_mfull_three(),
+               get_msparse_three(),
+               get_vector_threeOR(),
+               get_vector_threeIN(),
+               get_which_three(),
+               get_vector_str2lang()) %>%
+  group_by(expr) %>%
+  summarize(median_time = median(time)) %>%
+  arrange(-median_time)
