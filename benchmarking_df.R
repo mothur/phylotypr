557e0bed377a99b65094f7d2db090094dfb0661c
Pat Schloss <pschloss@umich.edu>
2024-04-26 16:16:39 -0400

    Benchmarking database approaches

--- benchmarking_df.R
+++ benchmarking_df.R
@@ -2,6 +2,8 @@ library(microbenchmark)
 library(tidyverse)
 library(data.table)
 library(Matrix)
+library(duckdb)
+library(duckplyr)
 
 set.seed(19760620)
 
@@ -79,12 +81,22 @@ tbl_prealloc_index <- function(n){
   df
 }
 
-dt_prealloc_index <- function(n){
+dt_prealloc_index_orig <- function(n){
   df <- data.table(kmer = rep(0, n), genus = rep(0, n), count = rep(0, n))
   for(i in 1:n) {
     df[i, (colnames(df)) := list(sample(n_kmers, 1),
                                  sample(n_genera, 1),
                                  sample(10, 1))]
+  }
+  df
+}
+
+dt_prealloc_index_set <- function(n){
+  df <- data.table(kmer = rep(0, n), genus = rep(0, n), count = rep(0, n))
+  for(i in 1:n) {
+    set(df, i, 1:3, list(sample(n_kmers, 1),
+                         sample(n_genera, 1),
+                         sample(10, 1)))
   }
   df
 }
@@ -136,7 +148,7 @@ matrix_predefine <- function(n) {
   m
 }
 
-matrix_sparseC <- function(n){
+matrix_sparseC_old <- function(n){
   kmer <- sample(n_kmers, n, replace = TRUE)
   genus <- sample(n_genera, n, replace = TRUE)
   count <- sample(10, n, replace = TRUE)
@@ -151,7 +163,16 @@ matrix_sparseC <- function(n){
 
 }
 
-matrix_sparseR <- function(n){
+matrix_sparseC <- function(n){
+  kmer <- sample(n_kmers, n, replace = TRUE)
+  genus <- sample(n_genera, n, replace = TRUE)
+  count <- sample(10, n, replace = TRUE)
+
+  m <- sparseMatrix(i = kmer, j = genus, x = count,
+                    repr = 'C')
+}
+
+matrix_sparseR_old <- function(n){
   kmer <- sample(n_kmers, n, replace = TRUE)
   genus <- sample(n_genera, n, replace = TRUE)
   count <- sample(10, n, replace = TRUE)
@@ -163,10 +184,19 @@ matrix_sparseR <- function(n){
     m[kmer[i], genus[i]] <- count[i]
   }
   m
-
 }
 
-matrix_sparseT <- function(n){
+matrix_sparseR <- function(n){
+  kmer <- sample(n_kmers, n, replace = TRUE)
+  genus <- sample(n_genera, n, replace = TRUE)
+  count <- sample(10, n, replace = TRUE)
+
+  m <- sparseMatrix(i = kmer, j = genus, x = count,
+                    repr = 'R')
+}
+
+
+matrix_sparseT_old <- function(n){
   kmer <- sample(n_kmers, n, replace = TRUE)
   genus <- sample(n_genera, n, replace = TRUE)
   count <- sample(10, n, replace = TRUE)
@@ -179,6 +209,16 @@ matrix_sparseT <- function(n){
   }
   m
 
+}
+
+
+matrix_sparseT <- function(n){
+  kmer <- sample(n_kmers, n, replace = TRUE)
+  genus <- sample(n_genera, n, replace = TRUE)
+  count <- sample(10, n, replace = TRUE)
+
+  m <- sparseMatrix(i = kmer, j = genus, x = count,
+                    repr = 'T')
 }
 
 Rcpp::sourceCpp("benchmarking_df.cpp")
@@ -194,8 +234,12 @@ microbenchmark(df_grow_rbind(n),
                tbl_prealloc_index(n),
                tbl_predefine(n),
                dt_grow_rbind(n),
-               dt_prealloc_index(n),
+               dt_prealloc_index_orig(n),
+               dt_prealloc_index_set(n),
                dt_predefine(n),
+               matrix_sparseC_old(n),
+               matrix_sparseR_old(n),
+               matrix_sparseT_old(n),
                matrix_sparseC(n),
                matrix_sparseR(n),
                matrix_sparseT(n),
@@ -207,36 +251,53 @@ microbenchmark(df_grow_rbind(n),
                ) %>%
   group_by(expr) %>%
   summarize(median_time = median(time)) %>%
-  arrange(-median_time)
+  arrange(-median_time) %>%
+  print(n = Inf)
 
-n <- 1000
+n <- 1e7
 set.seed(19760620)
 df <- df_predefine(n)
 set.seed(19760620)
 dt <- dt_predefine(n)
+dt2 <- copy(dt)
+setkey(dt2, kmer)
+
+con <- dbConnect(duckdb())
+dbWriteTable(con, "duck", df)
+
+duck <- as_duckplyr_df(df)
+
 set.seed(19760620)
 tbl <- tbl_predefine(n)
 set.seed(19760620)
 mfull <- matrix_predefine(n)
 set.seed(19760620)
-msparse <- matrix_sparseT(n)
+msparse <- sparseMatrix(df$kmer, df$genus, x = df$count)
 set.seed(19760620)
 lst <- list_predefine(n)
 
 get_df_single <- function(){
-  df[df$kmer == 20,]
+  df[which(df$kmer == 20),]
 }
 
 get_df_three <- function(){
-  df[df$kmer == 20 | df$kmer == 30 | df$kmer == 50,]
+  df[which(df$kmer == 20 | df$kmer == 30 | df$kmer == 50),]
 }
 
 get_dt_single <- function(){
-  dt[kmer == 20,]
+  dt[which(kmer == 20),]
 }
 
 get_dt_three <- function(){
-  dt[kmer == 20 | kmer == 30 | kmer == 50,]
+  dt[which(kmer == 20 | kmer == 30 | kmer == 50),]
+}
+
+get_dt_singlek <- function(){
+  dt2[.(20)]
+}
+
+get_dt_threek <- function(){
+  dt2[.(c(20, 50, 30)),]
 }
 
 get_tbl_single <- function(){
@@ -245,6 +306,22 @@ get_tbl_single <- function(){
 
 get_tbl_three <- function(){
   filter(tbl, kmer == 20 | kmer == 30 | kmer == 50)
+}
+
+get_duck_single <- function(){
+  filter(duck, kmer == 20)
+}
+
+get_duck_three <- function(){
+  filter(duck, kmer == 20 | kmer == 30 | kmer == 50)
+}
+
+get_dbi_single <- function(){
+  dbGetQuery(con, "SELECT * FROM duck WHERE kmer == 20")
+}
+
+get_dbi_three <- function(){
+  dbGetQuery(con, "SELECT * FROM duck WHERE kmer == 20 OR kmer == 30 OR kmer == 50")
 }
 
 get_tbl_threeJ <- function(){
@@ -317,8 +394,11 @@ get_which_three <- function(){
 }
 
 microbenchmark(get_df_single(),
+               get_duck_single(),
                get_dt_single(),
+               get_dt_singlek(),
                get_tbl_single(),
+               get_dbi_single(),
                get_mfull_single(),
                get_msparse_single(),
                get_vector_single(),
@@ -326,7 +406,10 @@ microbenchmark(get_df_single(),
 
                get_df_three(),
                get_dt_three(),
+               get_dt_threek(),
+               get_dbi_three(),
                get_tbl_three(),
+               get_duck_three(),
                get_tbl_threeJ(),
                get_mfull_three(),
                get_msparse_three(),
@@ -336,4 +419,6 @@ microbenchmark(get_df_single(),
                get_vector_str2lang()) %>%
   group_by(expr) %>%
   summarize(median_time = median(time)) %>%
-  arrange(-median_time)
+  arrange(-median_time) %>%
+  print(n = Inf)
+
